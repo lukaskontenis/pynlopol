12b96912a5e907b5aba8eea0e7270a132405efeb
lukaskontenis <lukas.kontenis@lightcon.com>
2020-10-14 09:13:04 +0300

    New functions get_waveplate_thickness, get_waveplate_retardance and plot_waveplate_response

--- lcmicro/polarimetry.py
+++ lcmicro/polarimetry.py
@@ -11,6 +11,7 @@ from numpy import zeros, sin, cos, pi
 import matplotlib.pyplot as plt
 
 from lklib.util import isstring
+from lklib.plot import add_x_marker, add_y_marker
 
 
 def col_vec(arr):
@@ -143,6 +144,75 @@ def get_stokes_vec(state):
 
     svec.shape = (4, 1)
     return svec
+
+
+def get_waveplate_thickness(
+        plate_type='hwp', wavl=None, biref=0.0092, worder=0):
+    """Get waveplate thickness given its order and birefringence.
+
+    Default birefringence is 0.0092 for a quartz waveplate in green. Thickess
+    is returned in the same units as wavelength.
+    """
+    if plate_type is 'hwp':
+        fac = 1
+    elif plate_type is 'qwp':
+        fac = 0.5
+    else:
+        print("Unsupported plate type " + plate_type)
+        return None
+    return (2*worder + 1)*wavl*fac / (2*biref)
+
+
+def get_waveplate_retardation(wavl=None, biref=0.0092, thickness=None):
+    """Get waveplate retardation given its birefringence and thickness.
+
+    Default birefringence is 0.0092 for a quartz waveplate in green.
+    Retartadtion is returned in waves.
+    """
+    rtrd = (2*biref*thickness/wavl)  # in terms of pi
+    rtrd = rtrd - 2*np.floor(rtrd/2)  # Subtract full waves, in terms of pi
+    rtrd = rtrd/2  # Retardance in terms of waves
+    return rtrd
+
+
+def plot_waveplate_response(
+        plate_type='hwp', rtrd=None, title_str=None, finalize_figure=True):
+    """Plot intensity transited through a rotatating waveplate and a polarizer.
+
+    """
+    if rtrd is None:
+        if plate_type is 'hwp':
+            rtrd = 0.5
+        elif plate_type is 'qwp':
+            rtrd = 0.25
+        else:
+            print("Unsupported plate type " + plate_type)
+            return None
+
+    in_svec = get_stokes_vec('hlp')
+    pol_hwp = get_mueller_mat('pol')
+
+    theta_arr = np.linspace(0, 2*np.pi, 500)
+    det_ampl = np.empty_like(theta_arr)
+
+    for ind, theta in enumerate(theta_arr):
+        hwp_mat = get_mueller_mat('rtd', theta=theta, d=rtrd*2*np.pi)
+        out_svec = pol_hwp.dot(hwp_mat.dot(in_svec))
+        det_ampl[ind] = out_svec[0]
+
+    plt.plot(theta_arr/np.pi*180, det_ampl)
+
+    if finalize_figure:
+        add_y_marker(0)
+        add_y_marker(1)
+        plt.xlim([0, 360])
+        plt.grid('on')
+        plt.xticks(np.arange(0, 361, 45))
+        if title_str is None:
+            title_str = 'Rotating {:s} response'.format(plate_type.upper())
+        plt.title(title_str)
+        plt.xlabel('{:s} orientation, deg'.format(plate_type.upper()))
+        plt.ylabel('Transmitted power, a.u.')
 
 
 def test_pol_trans():
