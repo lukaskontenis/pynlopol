30592f749e7b22682330414b4132615a82e4723a
Lukas Kontenis <dse.ssd@gmail.com>
2020-12-22 17:20:07 +0200

    setup file updated to include scripts and tests. v0.1.2 released

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lcmicro",
-    version="0.1.0",
+    version="0.1.2",
     author="Lukas Kontenis",
     author_email="dse.ssd@gmail.com",
     description="A Python library for nonlinear microscopy and polarimetry.",
@@ -22,4 +22,17 @@ setuptools.setup(
         'lklib>=0.0.13'
     ],
     python_requires='>=3.6',
+        data_files=[
+        ('scripts', [
+        'scripts/calib_laser_power.py',
+        'scripts/gen_img_report.py',
+        'scripts/lcmicro_to_png_tiff.py',
+        'scripts/make_psf_figure.py',
+        'scripts/sim_collagen_anim.py',
+        'scripts/sim_zcq_pipo_anim.py']),
+        ('tests', [
+        'tests/pipo_8x8_pol_states.dat',
+        'tests/test_polarimetry_lin.py',
+        'tests/test_polarimetry_nl.py',
+        'tests/test_polarimetry_plot.py'])],
 )
