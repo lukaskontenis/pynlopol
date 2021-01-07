17d7d1279ee09be0ed3bdbaa16f0f576d523dfaf
Lukas Kontenis <dse.ssd@gmail.com>
2021-01-07 21:39:47 +0200

    New submodule polarimetry.fitdata with a new function get_parmap

---
+++ lcmicro/polarimetry/fitdata.py
@@ -0,0 +1,35 @@
+"""fitdata structure definition and routines.
+
+This module contains functions related to fitdata, which is the output data
+format for NSMP fitting functions found in nsmp_fit.py
+
+This module is part of lcmicro, a Python library for nonlinear microscopy and
+polarimetry.
+
+Copyright 2015-2020 Lukas Kontenis
+Contact: dse.ssd@gmail.com
+"""
+import numpy as np
+
+
+def get_parmap(fitdata, par_name='x', parind=0):
+    """Make a 2D parameter map from fitdata."""
+    fit_result = fitdata['result']
+    fit_mask = fitdata['mask']
+    num_row, num_col = np.shape(fit_mask)
+
+    parmap = np.ndarray([num_row, num_col])
+    parmap.fill(np.nan)
+    indpx = 0
+    for ind_row in range(num_row):
+        for ind_col in range(num_col):
+            if indpx == fitdata['num_pts_to_fit']:
+                break
+            if fit_mask[ind_row, ind_col]:
+                if par_name == 'x':
+                    parmap[ind_row, ind_col] = fit_result[indpx].x[parind]
+                elif par_name == 'err':
+                    parmap[ind_row, ind_col] = fit_result[indpx].fun[parind]
+                indpx += 1
+
+    return parmap
