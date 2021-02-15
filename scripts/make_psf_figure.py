f6af43f593f99c146aa0471d3fe48edae8cd98c8
Lukas Kontenis <dse.ssd@gmail.com>
2021-02-15 13:29:57 +0200

    make_psf_figure script now supports drag and drop

--- scripts/make_psf_figure.py
+++ scripts/make_psf_figure.py
@@ -1,31 +1,50 @@
-"""Make PSF figure.
+"""Make a PSF figure.
 
-Make a THG Z PSF figure from a line scan data file.
+Make an axial THG Z PSF figure from a line scan data file.
 
 If the trace does not reach zero due to background you can disable zero line
-plotting by setting show_y_zero to False.
+plotting by setting show_y_zero_marker to False.
 
 This script is part of lcmicro, a Python library for nonlinear microscopy and
 polarimetry.
 
-Copyright 2015-2020 Lukas Kontenis
+Copyright 2015-2021 Lukas Kontenis
 Contact: dse.ssd@gmail.com
 """
 # flake8: noqa
 # pylint: skip-file
 
-print("=== lcmicro ===")
-print("Generating PSF figure...")
+import sys
+import os
+import pathlib
 
 from lklib.util import handle_general_exception
-from lcmicro.report import gen_thg_psf_fig
 
-try:
-    gen_thg_psf_fig(
-        file_name='800_PSF.txt', wavl=0.8, show_y_zero_marker=False,
-        suptitle_suffix='Second surface, LCM1, CRONUS VIS beam')
-except Exception:
-    handle_general_exception("Could not generate figure")
+from lcmicro.report import gen_thg_psf_fig
+from lcmicro.dataio import get_psf_data_file_name
+
+print("=== PSF figure generator ===")
+
+file_name = None
+num_args = len(sys.argv)
+if num_args < 2:
+    file_name = get_psf_data_file_name()
+else:
+    file_name = sys.argv[1]
+
+if file_name is None:
+    print("No input provided. Specify a file name using:")
+    print("\t" + os.path.basename(__file__) + " psf.txt")
+    print("\nOr drag a TXT file on the script.\n")
+else:
+    try:
+        print("Generating PSF figure for '{:s}'...".format(
+            pathlib.Path(file_name).absolute().name))
+
+        gen_thg_psf_fig(
+            file_name=file_name, wavl=1.03, show_y_zero_marker=False,
+            suptitle_suffix='Second surface')
+    except Exception:
+        handle_general_exception("Conversion failed")
 
 input("Pess any key to close this window...")
-
