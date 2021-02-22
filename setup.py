bfee0078aa39bd4d308447fe53007e9ad42663f7
Lukas Kontenis <dse.ssd@gmail.com>
2021-02-22 12:32:57 +0200

    v0.5.3 released

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lcmicro",
-    version="0.5.2",
+    version="0.5.3",
     author="Lukas Kontenis",
     author_email="dse.ssd@gmail.com",
     description="A Python library for nonlinear microscopy and polarimetry.",
