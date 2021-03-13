46796e830e2856b0100ca4617fe5580185fe9a36
Lukas Kontenis <dse.ssd@gmail.com>
2021-03-13 23:42:42 +0200

    Improved nonlinear polarimetry testing

--- tests/test_polarimetry_nl.py
+++ tests/test_polarimetry_nl.py
@@ -77,17 +77,9 @@ class TestPolarimetry(unittest.TestCase):
 
         pipo_data = simulate_pipo(**par)
 
+        # This array was validated against AG C6 PIPO formula and NLPS v2.28 on
+        # 2021.03.11
         ref_pipo = np.array([
-            [0.   , 0.5  , 1.   , 0.5  , 0.   , 0.5  , 1.   , 0.5  ],
-            [0.146, 1.132, 1.966, 1.438, 0.33 , 0.012, 0.198, 0.059],
-            [0.5  , 1.585, 2.531, 2.277, 1.125, 0.259, 0.031, 0.067],
-            [0.854, 1.593, 2.364, 2.524, 1.92 , 1.097, 0.596, 0.52 ],
-            [1.   , 1.152, 1.563, 2.036, 2.25 , 2.036, 1.562, 1.152],
-            [0.854, 0.52 , 0.596, 1.097, 1.92 , 2.524, 2.364, 1.593],
-            [0.5  , 0.067, 0.031, 0.259, 1.125, 2.277, 2.531, 1.585],
-            [0.146, 0.059, 0.198, 0.012, 0.33 , 1.438, 1.966, 1.132]])
-
-        ref_pipo_90ofs = np.array([
             [2.25 , 2.036, 1.562, 1.152, 1.   , 1.152, 1.562, 2.036],
             [1.92 , 2.524, 2.364, 1.593, 0.854, 0.52 , 0.596, 1.097],
             [1.125, 2.277, 2.531, 1.585, 0.5  , 0.067, 0.031, 0.259],
@@ -97,11 +89,36 @@ class TestPolarimetry(unittest.TestCase):
             [1.125, 0.259, 0.031, 0.067, 0.5  , 1.585, 2.531, 2.277],
             [1.92 , 1.097, 0.596, 0.52 , 0.854, 1.593, 2.364, 2.524]])
 
-        test = tensor_eq(pipo_data, ref_pipo, thr=1E-3)
-        test_90ofs = tensor_eq(pipo_data, ref_pipo_90ofs, thr=1E-3)
+        # This array was validated against AG C6 PIPO formula and NLPS v2.28 on
+        # 2021.03.11
+        ref_pipo_90ofs = np.array([
+            [0.   , 0.5  , 1.   , 0.5  , 0.   , 0.5  , 1.   , 0.5  ],
+            [0.146, 1.132, 1.966, 1.438, 0.33 , 0.012, 0.198, 0.059],
+            [0.5  , 1.585, 2.531, 2.277, 1.125, 0.259, 0.031, 0.067],
+            [0.854, 1.593, 2.364, 2.524, 1.92 , 1.097, 0.596, 0.52 ],
+            [1.   , 1.152, 1.563, 2.036, 2.25 , 2.036, 1.562, 1.152],
+            [0.854, 0.52 , 0.596, 1.097, 1.92 , 2.524, 2.364, 1.593],
+            [0.5  , 0.067, 0.031, 0.259, 1.125, 2.277, 2.531, 1.585],
+            [0.146, 0.059, 0.198, 0.012, 0.33 , 1.438, 1.966, 1.132]])
+
+        # Simulated PIPO instensity scales with ratio and can also be influenced
+        # by amplitude normalization. It is likely safe to compare normalized
+        # PIPO arrays.
+        allow_free_pipo_amplitude = True
+        if allow_free_pipo_amplitude:
+            test = tensor_eq(pipo_data/np.max(pipo_data),
+                             ref_pipo/np.max(ref_pipo), thr=1E-3)
+            test_90ofs = tensor_eq(pipo_data/np.max(pipo_data),
+                                   ref_pipo_90ofs/np.max(ref_pipo_90ofs),
+                                   thr=1E-3)
+        else:
+            test = tensor_eq(pipo_data, ref_pipo, thr=1E-3)
+            test_90ofs = tensor_eq(pipo_data, ref_pipo_90ofs, thr=1E-3)
 
         if test_90ofs:
-            print("WARNING: PIPO map test shows a 90° delta offset. This may indicate a primary X/Z axis inconsistency due to a mixing of DSMP/TSMP/NSMP frameworks.")
+            print("WARNING: PIPO map test shows a 90° delta offset. This may "
+                  "indicate a primary X/Z axis inconsistency due to a mixing "
+                  "of DSMP/TSMP/NSMP frameworks.")
 
         self.assertTrue(test or test_90ofs)
 
