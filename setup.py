f00fb9bfbc2e2113ff25636b876806fbe6969d62
Lukas Kontenis <dse.ssd@gmail.com>
2022-11-22 12:35:31 +0200

    Release v1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pynlopol",
-    version="1.0.1",
+    version="1.0.2",
     author="Lukas Kontenis",
     author_email="dse.ssd@gmail.com",
     description="A Python library for nonlinear polarimetry.",
